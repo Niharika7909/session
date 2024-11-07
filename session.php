e3ad11e6c6fe460ea6643ff755233c0be5316bca
Niharika7909 <niharikasharma7909@gmail.com>
2024-11-07 21:20:23 +0530

    session.php

---
+++ session.php
@@ -0,0 +1,10 @@
+<?php
+// what is a session?
+//used to manage information across difference pages
+//verify the user login info
+session_start();
+$_SESSION[ 'username' ]="Niharika";
+$_SESSION[ 'favcat' ]="Niharika";
+echo"we have saved your session";
+
+?>
