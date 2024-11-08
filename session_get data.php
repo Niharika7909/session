320bad018343d63d4299e1dc42f9a1180d3c6203
Niharika7909 <niharikasharma7909@gmail.com>
2024-11-08 09:59:19 +0530

    Update session_get data.php
       echo"<br> your favorite category is ".$_SESSION(['favCat']);
    change  :-    .$_SESSION(['favcat']);

--- session_get data.php
+++ session_get data.php
@@ -3,7 +3,7 @@
 session_start();
 if(isset($_SESSION['username'])){
     echo"Welcome".$_SESSION['username'];
-    echo"<br> your favorite category is ".$_SESSION(['favCat']);
+    echo"<br> your favorite category is ".$_SESSION(['favcat']);
     echo"<br>";
 }
 else{
